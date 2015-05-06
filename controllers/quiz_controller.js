c5f6a63008d60d637081a0af4f7cee836e82ef4d
Andrax92 <e.vacas@alumnos.upm.es>
2015-05-06 21:57:58 +0200

    Crear pregunta y validación

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -11,20 +11,20 @@ exports.load = function(req, res, next, quizId) {
 				next(new Error('No existe quizId = ' + quizId));
 			}
 		}
-	).catch(function(error) { next(error); });
+	).catch(function(error) { next(error)});
 };
 
 // GET /quizes
 exports.index = function (req, res) {
     models.Quiz.findAll().then(function (quizes) {
-        res.render('quizes/index', { quizes: quizes });
+        res.render('quizes/index.ejs', { quizes: quizes, errors: [] });
     }
-  ).catch(function(error) { next(error); });
+  ).catch(function(error) { next(error)});
 };
 
 // GET /quizes/:id
 exports.show = function (req, res) {
-		res.render('quizes/show', { quiz: req.quiz });
+		res.render('quizes/show', { quiz: req.quiz, errors: [] });
 };
 
 // GET /quizes/:id/answer
@@ -33,5 +33,35 @@ exports.answer = function (req, res) {
         if (req.query.respuesta === req.quiz.respuesta) {
         	resultado = 'Correcto';
         }
-	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+	res.render('quizes/answer', {
+		quiz: req.quiz,
+		respuesta: resultado,
+		errors: []});
+};
+
+// GET /quizes/new
+exports.new = function(req, res) {
+	 // Crea objeto quiz
+	var quiz = models.Quiz.build(
+	{pregunta: "Pregunta", respuesta: "Respuesta"});
+	
+	res.render('/quizes/new', { quiz: quiz, errors: []});
+};
+
+// POST /quizes/create
+exports.create = function(req, res) {
+	var quiz = models.Quiz.build( req.body.quiz );
+	
+	// guarda en la DB los campos pregunta y respuesta
+	quiz.validate().then(
+		function(err){
+			if(err) {
+				res.render('/quizes/new', {quiz: quiz, errors: err.errors});
+			} else {
+				quiz.save({fields: ["pregunta", "respuesta"]})
+				.then( function(){
+					res.redirect('/quizes')
+				})
+			}
+		}); //Redirección HTTP a lista de preguntas (URL relativo)
 };
--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,12 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors: [] });
 });
 
 /*GET /author */
 router.get('/author', function(req, res) {
-    res.render('author', { title: 'Créditos' });
+    res.render('author', { title: 'Créditos', errors: [] });
 });
 
 // Autoload de comandos con :quizId
@@ -21,5 +21,7 @@ router.param('quizId', quizController.load);
 router.get('/quizes',					   quizController.index);
 router.get('/quizes/:quizId(\\d+)',  	   quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new',				   quizController.new);
+router.post('/quizes/create',			   quizController.create);
 
 module.exports = router;
